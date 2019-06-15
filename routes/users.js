994b02d8346a5a3cebc35b288dc27981e21a43e0
Packet <199sd@naver.com>
2019-06-15 13:32:38 +0900

    메인페이지 렌더

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const user = require('../models/userModel');
+const crypto = require('crypto');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  res.render('login.html');
 });
+
+router.post('/',(req,res,next)=>{
+  user.findOne({ID:req.body.ID, PW:crypto.createHash('sha512').update(req.body.PW).digest('base64')})
+  .then((result) => {
+    if(result) {
+      req.session.ID = req.body.ID;
+      res.redirect('/main');
+    }
+  }).catch((err) => {
+    
+  });
+})
 
 module.exports = router;
